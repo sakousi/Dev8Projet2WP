be38880582e2a692658bfa7a8f52dc3423f73bfa
theau <theaudugueperoux@gmail.com>
2023-02-08 15:18:53 +0100

    :lipstick: footer style

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php wp_footer(); ?>
 <footer class="overflow-hidden bg-creamy">
-    <div class="mx-auto px-4 py-24 max-w-6xl relative w-full z-0 after:content after:absolute after:w-[200%] after:h-full after:skew-x-[-20deg] after:top-0 after:bg-white after:-z-10 after:-translate-x-[70px]">
-        <div class="w-full flex justify-between">
+    <div class="text-blue mx-auto px-4 py-24 max-w-6xl relative w-full z-0 after:content after:absolute after:w-[200%] after:h-full after:skew-x-[-20deg] after:top-0 after:bg-white after:-z-10 after:-translate-x-[70px]">
+        <div class="w-full flex justify-between pb-8">
             <div class="">
                 <img src="<?php the_field('logo', 'options'); ?>" alt="">
                 <p class="text-blue">
@@ -26,7 +26,7 @@
 	        <?php if(have_rows('campus', 'options')): ?>
 	    	    <?php while(have_rows('campus', 'options')): the_row() ?>
                     <div>
-                        <h3><?= get_sub_field('city') ?></h3>
+                        <h3 class="font-bold text-lg"><?= get_sub_field('city') ?></h3>
                         <?= get_sub_field('address') ?>
                     </div>
 	    	    <?php endwhile; ?>
@@ -34,11 +34,11 @@
         </div>
         <div class="flex justify-between">
             <div>
-                <h3>Nous contacter.</h3>
+                <h3 class="font-bold text-lg">Nous contacter.</h3>
                 <p><?= get_field('email', 'options') ?></p>
             </div>
             <div>
-                <h3>Nous suivre.</h3>
+                <h3 class="font-bold text-lg">Nous suivre.</h3>
                 <div class="flex flex-col">
                     <?php if(have_rows('social_media', 'options')): ?>
                         <?php while(have_rows('social_media', 'options')): the_row() ?>
@@ -48,11 +48,18 @@
                 </div>
             </div>
             <div>
-                <h3>En toute transparence.</h3>
+                <h3 class="font-bold text-lg">En toute transparence.</h3>
+                <div class="flex flex-col">
+                    <?php if(have_rows('transparency', 'options')): ?>
+                        <?php while(have_rows('transparency', 'options')): the_row() ?>
+                            <a href="<?= get_sub_field('page') ?>" ><?= get_sub_field('name') ?></a>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
             </div>
             <div>
-                <h3>Faire connaissance.</h3>
-                <button>Ecris-nous</button>
+                <h3 class="font-bold text-lg">Faire connaissance.</h3>
+                <button class="py-4 px-8 rounded-full bg-orange text-white">Ecris-nous</button>
             </div>
         </div>
     </div>
