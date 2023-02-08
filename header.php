17e34138cb92a0c1acc252cab1e75cf553f01510
theau <theaudugueperoux@gmail.com>
2023-02-08 16:37:49 +0100

    :poop: add conditional header colors

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<?php
+$logo = get_field('logo_nav', 'options');
+$classes = 'text-white text-lg';
+if (is_page(['contact', 'actualities'])) {
+    $logo = get_field('logo_nav_dark', 'options');
+    $classes ='text-blue text-lg';
+}
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -7,11 +16,10 @@
 	<?php wp_head(); ?>
 </head>
 
-
 <body <?php body_class();?>>
 <nav class="absolute !py-0 container flex inset-x-0">
     <a class="py-2 h-16" href="<?= get_home_url() ?>">
-        <img class="h-full" src="<?php the_field('logo_nav', 'options'); ?>" alt="">
+        <img class="h-full" src="<?= $logo ?>" alt="">
     </a>
     <ul class="w-full h-16 flex justify-center items-center inset-x-0">
         <?php
@@ -19,7 +27,7 @@
             $menuID = $menuLocations['menu_principal'];
             $menu = wp_get_nav_menu_items($menuID);
             foreach($menu as $navItem) {
-                echo '<li class="px-8"><a href="'.$navItem->url.'" title="'.$navItem->title.'" class="text-white text-lg">'.$navItem->title.'</a></li>';
+                echo '<li class="px-8"><a href="'.$navItem->url.'" title="'.$navItem->title.'" class="'.$classes.'">'.$navItem->title.'</a></li>';
             }
         ?>
     </ul>
