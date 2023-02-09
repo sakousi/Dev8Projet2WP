b87c05b3c4e4fbd53b5fc4fcfaa437da8da15d94
theau <theaudugueperoux@gmail.com>
2023-02-09 16:40:16 +0100

    :gun: Responsive beam

--- expertise.php
+++ expertise.php
@@ -1,18 +1,18 @@
 <?php /* Template Name: Expertise */ ?>
 <?php get_header(); ?>
 <div class="bg-blue w-full">
-	<header class="container text-white flex flex-col items-center w-1/2 text-center">
+	<header class="container text-white flex flex-col items-center md:w-1/2 text-center">
 		<p class="text-lg font-bold"><?php the_field('up_title'); ?></p>
-		<h1 class="py-8 text-4xl font-extrabold"><?php the_field('title'); ?></h1>
+		<h1 class="py-8 text-3xl md:text-4xl font-extrabold"><?php the_field('title'); ?></h1>
 		<p><?php the_field('desc'); ?></p>
 	</header>
 </div>
 	<main>
 		<section class="container text-blue">
-			<h2 class="py-8 text-4xl font-extrabold"><?php the_field('title_2'); ?></h2>
+			<h2 class="py-8 text-3xl md:text-4xl font-extrabold"><?php the_field('title_2'); ?></h2>
 			<p><?php the_field('desc_2'); ?></p>
 			<?php if(have_rows('repeater_2')): ?>
-				<div class="grid grid-cols-2 gap-8">
+				<div class="grid md:grid-cols-2 grid-cols-1 gap-8">
 					<?php while (have_rows('repeater_2')) : the_row() ?>
 						<div>
 							<img src="<?php the_sub_field('icon'); ?>" alt="">
@@ -25,13 +25,13 @@
 		</section>
 		<div class="bg-blue w-full">
 			<section class="container text-white">
-                <div class="w-1/2 pb-10">
+                <div class="md:w-1/2 pb-10">
 				    <p class="text-lg font-bold"><?php the_field('up_title_3'); ?></p>
-				    <h2 class="py-8 text-4xl font-extrabold"><?php the_field('title_3'); ?></h2>
+				    <h2 class="py-8 text-3xl md:text-4xl font-extrabold"><?php the_field('title_3'); ?></h2>
 				    <p><?php the_field('desc_3'); ?></p>
                 </div>
 				<?php if(have_rows('repeater_3')): ?>
-					<div class="grid grid-cols-4 gap-16">
+					<div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-16">
 						<?php while (have_rows('repeater_3')) : the_row() ?>
 							<div>
 								<h3 class="text-xl font-bold"><?php the_sub_field('title'); ?></h3>
--- willtobe.php
+++ willtobe.php
@@ -1,16 +1,16 @@
 <?php /* Template Name: WillToBe */ ?>
 <?php get_header(); ?>
 	<div class="bg-blue w-full">
-		<header class="container flex justify-center items-center flex-col !py-48 w-2/3">
+		<header class="container flex justify-center items-center flex-col !py-48 md:w-2/3">
 				<p class="text-lg text-white font-bold text-center"><?= get_field('up_title'); ?></p>
-				<h1 class="text-5xl font-extrabold font-khula text-white text-center py-12"><?php the_field('title'); ?></h1>
+				<h1 class="md:text-5xl text-4xl font-extrabold font-khula text-white text-center py-12"><?php the_field('title'); ?></h1>
 		</header>
 	</div>
 	<section class="container text-blue">
 		<p class="font-bold text-lg"><?php the_field('up_title_2'); ?></p>
-		<h2 class="font-extrabold text-4xl w-1/2 py-12"><?php the_field('title_2'); ?></h2>
+		<h2 class="font-extrabold md:text-4xl text-3xl md:w-1/2 py-12"><?php the_field('title_2'); ?></h2>
 		<?php if(have_rows('repeater_2')): ?>
-			<div class="grid grid-cols-2 gap-8">
+			<div class="grid sm:grid-cols-2 grid-cols-1 gap-8">
 				<?php while(have_rows('repeater_2')): the_row(); ?>
 					<div class="">
 						<h3 class="font-bold text-lg"><?php the_sub_field('title'); ?></h3>
@@ -23,7 +23,7 @@
 	<div class="bg-blue w-full">
 		<section class="container flex flex-col text-white">
 			<p class="font-bold text-lg"><?php the_field('up_title_3'); ?></p>
-			<h2 class="font-extrabold text-4xl w-1/2 py-12"><?php the_field('title_3'); ?></h2>
+			<h2 class="font-extrabold md:text-4xl text-3xl md:w-1/2 py-12"><?php the_field('title_3'); ?></h2>
 			<?= get_field('desc_3') ?>
 		</section>
 	</div>
@@ -31,11 +31,11 @@
 	    <section class="container text-blue">
             <div class="flex flex-col items-center">
                 <p class="font-bold text-lg text-center"><?php the_field('up_title_4'); ?></p>
-                <h2 class="font-extrabold text-4xl w-1/2 py-12 text-center"><?php the_field('title_4'); ?></h2>
+                <h2 class="font-extrabold md:text-4xl text-3xl md:w-1/2 py-12 text-center"><?php the_field('title_4'); ?></h2>
 	    	    <?php if(have_rows('repeater_4')): ?>
                     <div class="flex justify-center flex-wrap">
 	    	    		<?php while(have_rows('repeater_4')): the_row(); ?>
-                            <div class="basis-1/3">
+                            <div class="md:basis-1/3 sm:basis-1/2 basis-full pb-8">
                                 <h3 class="font-bold text-lg"><?php the_sub_field('title'); ?></h3>
                                 <p><?php the_sub_field('desc'); ?></p>
                             </div>
@@ -47,11 +47,11 @@
                 <img class="w-32" src="<?php the_field('separator_4'); ?>" alt="">
             </div>
             <div class="flex flex-col items-center">
-                <h3 class="font-extrabold text-4xl w-1/2 text-center py-12"><?php the_field('2_title_4'); ?></h3>
+                <h3 class="font-extrabold md:text-4xl text-3xl md:w-1/2 text-center py-12"><?php the_field('2_title_4'); ?></h3>
                 <?php if(have_rows('2_repeater_4')): ?>
-                    <div class="flex justify-center pt-12">
+                    <div class="flex justify-center flex-wrap pt-12">
                         <?php while(have_rows('2_repeater_4')): the_row(); ?>
-                            <div class="flex flex-col items-center basis-1/4 px-8 text-center">
+                            <div class="flex flex-col items-center md:basis-1/4 sm:basis-1/2 basis-full px-8 text-center">
                                 <img src="<?php the_sub_field('icon'); ?>" alt="">
                                 <h4 class="font-bold text-lg text-center"><?php the_sub_field('title'); ?></h4>
                                 <?= get_sub_field('desc'); ?>
@@ -64,9 +64,9 @@
     </div>
     <section class="container text-blue flex flex-col items-center text-center">
         <p class="font-bold text-lg"><?php the_field('up_title_5'); ?></p>
-        <h2 class="font-extrabold text-4xl w-1/2 py-12"><?php the_field('title_5'); ?></h2>
+        <h2 class="font-extrabold md:text-4xl text-3xl md:w-1/2 py-12"><?php the_field('title_5'); ?></h2>
             <?php if(have_rows('repeater_5')): ?>
-                <div class="grid grid-cols-4 gap-8">
+                <div class="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-8">
                     <?php while(have_rows('repeater_5')): the_row(); ?>
                         <div class="">
                             <div class="h-74 w-full">
